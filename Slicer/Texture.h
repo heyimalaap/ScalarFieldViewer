3cc9bcf6b35eeda2134425872d8dcfb68f84bf5d
Alaap Surendran <alaapsurendran@gmail.com>
2024-11-01 16:54:06 +0530

    Move Texture classes to their own file

---
+++ Slicer/Texture.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <GL/gl.h>
+#include <glm/glm.hpp>
+#include <VTKParser.h>
+
+class Texture1D {
+public:
+    Texture1D();
+    Texture1D(GLuint id, int L);
+
+    static Texture1D from_colormap(glm::vec3 low_color, glm::vec3 high_color);
+    void bind();
+private:
+    GLuint m_id;
+    int m_L;
+};
+
+class Texture3D {
+public:
+    Texture3D();
+    Texture3D(GLuint id, int L, int W, int D);
+
+    static Texture3D from_data(VTKField<double> data, size_t width, size_t height, size_t depth);
+    void bind();
+    GLuint id() const;
+    Dimension dimension() const;
+
+private:
+    GLuint m_id;
+    int m_L, m_W, m_D;
+};
+
--- Slicer/main.cpp
+++ Slicer/main.cpp
@@ -1,5 +1,6 @@
 #include "ArcballCamera.h"
 #include "ShaderProgram.h"
+#include "Texture.h"
 #include <GL/gl.h>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
@@ -20,105 +21,6 @@
 #include <imgui.h>
 #include <imgui_impl_glfw.h>
 #include <imgui_impl_opengl3.h>
-
-class Texture1D {
-public:
-    Texture1D() {}
-
-    Texture1D(GLuint id, int L)
-        : m_id(id), m_L(L) {}
-
-    static Texture1D from_colormap(glm::vec3 low_color, glm::vec3 high_color)
-    {
-        std::vector<float> colormap;
-        for (int i = 0; i < 256; i++)
-        {
-            float t = i / 255.0f;
-            glm::vec3 c = glm::mix(low_color, high_color, t);
-            colormap.push_back(c.r);
-            colormap.push_back(c.g);
-            colormap.push_back(c.b);
-        }
-        
-        GLuint id;
-        glGenTextures(1, &id);
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_1D, id);
-        glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-        glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        glTexImage1D(GL_TEXTURE_1D, 0, GL_RGB, 256, 0, GL_RGB, GL_FLOAT, colormap.data());
-        glBindTexture(GL_TEXTURE_1D, 0);
-
-        return Texture1D(id, 256);
-    }
-
-    void bind()
-    {
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_1D, m_id);
-    }
-
-private:
-    GLuint m_id;
-    int m_L;
-};
-
-class Texture3D {
-public:
-    Texture3D() {}
-
-    Texture3D(GLuint id, int L, int W, int D)
-        : m_id(id), m_L(L), m_W(W), m_D(D) {}
-
-    static Texture3D from_data(VTKField<double> data, size_t width, size_t height, size_t depth)
-    {
-        std::vector<float> fdata;
-        for (size_t z = 0; z < depth; z++)
-        {
-            for (size_t y = 0; y < height; y++)
-            {
-                for (size_t x = 0; x < width; x++)
-                {
-                    fdata.push_back(data(x, y, z));
-                }
-            }
-        }
-        GLuint id;
-        glGenTextures(1, &id);
-        glActiveTexture(GL_TEXTURE0 + 1);
-        glBindTexture(GL_TEXTURE_3D, id);
-
-        glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-        glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-        glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
-        glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-        glTexImage3D(GL_TEXTURE_3D, 0, GL_R32F, width, height, depth, 0, GL_RED, GL_FLOAT, fdata.data());
-
-        glBindTexture(GL_TEXTURE_3D, 0);
-
-        return Texture3D(id, width, height, depth);
-    }
-
-    void bind()
-    {
-        glActiveTexture(GL_TEXTURE0 + 1);
-        glBindTexture(GL_TEXTURE_3D, m_id);
-    }
-
-    GLuint getID() const { return m_id; }
-
-    Dimension dimension() const 
-    { 
-        return Dimension { m_L, m_W, m_D };
-    }
-
-private:
-    GLuint m_id;
-    int m_L, m_W, m_D;
-};
 
 class WireframeBoundingBox
 {
